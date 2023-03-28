a41d10ae4b12e35628214e4e3202e64d8abcc5f1
alej098 <125920247+alej098@users.noreply.github.com>
2023-03-28 14:14:26 -0500

    Add files via upload

---
+++ Hellos/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("inr");
+        System.out.println("long");
+        System.out.println("double");
+        System.out.println("boolean");
+        System.out.println("String");
+        System.out.println("enteros");
+        System.out.println("decimales");
+        System.out.println("texto");
+
+    }
+}
